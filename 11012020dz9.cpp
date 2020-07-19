a8800b227ec2c0799f6d927ee76b8656b57dd131
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:13:08 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz9.cpp
@@ -0,0 +1,20 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры диаметр окружности.Посчитать площадь и периметр окружности.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	double pi, r, s, d, p;
+	cout << "Введите диаметр окружности : ";
+	cin >> d;
+	pi = 3.1415;
+	r = d / 2;
+	s = pi * (r * r);
+	p = 2 * pi * r;
+	cout << "Площадь окружности = " << s << endl;
+	cout << "Периметр окружности = " << p << endl;
+}
